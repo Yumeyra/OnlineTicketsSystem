43a27df4ac14872bee7d3c31ec1ed1fb28b02055
Yumeyra <yumeyrainova@gmail.com>
2026-03-05 09:47:03 +0200

    update home.css and site.css

--- OnlineTicketsSystem/ViewComponents/CitiesMenuViewComponent.cs
+++ OnlineTicketsSystem/ViewComponents/CitiesMenuViewComponent.cs
@@ -1,11 +1,41 @@
 ﻿
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using OnlineTicketsSystem.Data;
+
+//namespace OnlineTicketsSystem.ViewComponents
+//{
+//    public class CitiesMenuViewComponent :ViewComponent
+//    {
+//        private readonly ApplicationDbContext _context;
+
+//        public CitiesMenuViewComponent(ApplicationDbContext context)
+//        {
+//            _context = context;
+//        }
+
+
+
+
+//        public async Task<IViewComponentResult> InvokeAsync()
+//        {
+//            var cities = await _context.Cities
+//                .OrderBy(c => c.Name)
+//                .ToListAsync();
+
+//            return View(cities);
+//        }
+
+//    }
+//}
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineTicketsSystem.Data;
+using OnlineTicketsSystem.ViewModels;
 
 namespace OnlineTicketsSystem.ViewComponents
 {
-    public class CitiesMenuViewComponent :ViewComponent
+    public class CitiesMenuViewComponent : ViewComponent
     {
         private readonly ApplicationDbContext _context;
 
@@ -14,18 +44,36 @@ namespace OnlineTicketsSystem.ViewComponents
             _context = context;
         }
 
-
-
-
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var cities = await _context.Cities
-                .OrderBy(c => c.Name)
+                .OrderBy(c => c.Region)
+                .ThenBy(c => c.Name)
+                .Select(c => new { c.Name, c.Slug, c.Region })
                 .ToListAsync();
 
-            return View(cities);
-        }
+            var vm = new CitiesMenuVm();
 
+            vm.Regions = cities
+                .Select(c => c.Region)
+                .Where(r => !string.IsNullOrWhiteSpace(r))
+                .Distinct()
+                .OrderBy(r => r)
+                .ToList();
+
+            foreach (var region in vm.Regions)
+            {
+                vm.CitiesByRegion[region] = cities
+                    .Where(c => c.Region == region)
+                    .Select(c => new CityMenuItemVm
+                    {
+                        Name = c.Name,
+                        Slug = c.Slug
+                    })
+                    .ToList();
+            }
+
+            return View(vm);
+        }
     }
 }
-
---
+++ OnlineTicketsSystem/ViewModels/CitiesMenuVm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineTicketsSystem.ViewModels
+{
+    public class CitiesMenuVm
+    {
+        public List<string> Regions { get; set; } = new();
+        public Dictionary<string, List<CityMenuItemVm>> CitiesByRegion { get; set; } = new();
+    }
+
+    public class CityMenuItemVm
+    {
+        public string Name { get; set; } = "";
+        public string Slug { get; set; } = "";
+    }
+}
